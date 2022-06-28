5b29f3159f4b5d303623ac761b2cc45efb4930a0
Ogu <august.gros@gmail.com>
2022-06-28 12:53:33 +0200

    add animation on scroll of nav and bio

---
+++ js/main_page.js
@@ -0,0 +1,14 @@
+const page = document.querySelector('#page');
+const nav = document.querySelector('#nav');
+const bio = document.querySelector('#bio');
+
+window.addEventListener('scroll', () => {
+    let scrollY = this.scrollY;
+    if(scrollY >= 775){
+        bio.classList.remove('invert');
+        nav.classList.add('text-white');
+    }else{
+        bio.classList.add('invert');
+        nav.classList.remove('text-white');
+    }
+})
