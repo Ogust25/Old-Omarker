2af8e000dbd3354156d7393ac2ba50e7d4a49a0c
Ogu <august.gros@gmail.com>
2022-06-30 14:11:12 +0200

    add router/htaccess + all pages

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+$url = explode('/', $_SERVER['REQUEST_URI']);
+switch ($url[2]) {
+    case '' :
+        require __DIR__ . '/accueil.html';
+        break;
+    case 'peinture' :
+        require __DIR__ . '/peinture.html';
+        break;
+    case 'tattoo' :
+        require __DIR__ . '/tattoo.html';
+        break;
+    case 'graphisme' :
+        require __DIR__ . '/graphisme.html';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/404.html';
+        break;
+}
