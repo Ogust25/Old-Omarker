af570ab2b047f1d92ae14f6558aa64047c0548f8
Ogu <august.gros@gmail.com>
2022-07-20 13:23:27 +0200

    finish integration of tattoo's form

--- js/tattoo_page.js
+++ js/tattoo_page.js
@@ -8,6 +8,7 @@ const dateModal = document.querySelector("#dateModal");
 const techModal = document.querySelector("#techModal");
 const menu = document.querySelector("#menuPeinture");
 const navMenu = document.querySelector("#navMenu");
+const nav = document.querySelector("#nav");
 const linkMenu = document.querySelectorAll(".linkMenu");
 
 /* Event pour ouvrir/fermer les boites modale */
@@ -55,10 +56,10 @@ menu.addEventListener("click", () => {
 });
 
 linkMenu.forEach((link) => {
-  link.addEventListener('click', () => {
+  link.addEventListener("click", () => {
     toggleMenu();
-  })
-})
+  });
+});
 
 /* Fonction pour ouvrir/fermer le menu sur telephone */
 const toggleMenu = () => {
@@ -73,4 +74,41 @@ const toggleMenu = () => {
     navMenu.classList.add("-bottom-50%");
     menu.firstChild.nodeValue = "MENU";
   }
+};
+
+/* Scrpit pour que le textarea resize auto */
+const tx = document.getElementsByTagName("textarea");
+for (let i = 0; i < tx.length; i++) {
+  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+  tx[i].addEventListener("input", OnInput, false);
 }
+
+function OnInput() {
+  this.style.height = "auto";
+  this.style.height = (this.scrollHeight) + "px";
+}
+
+/* Preview pour le flash choisis */
+const output = document.getElementById("previewFlash");
+const flashFile = document.querySelector("#flashChoisi");
+
+flashFile.addEventListener("change", (event) => {
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function () {
+    URL.revokeObjectURL(output.src); // free memory
+  };
+});
+
+/* Evenement au scroll */
+window.addEventListener("scroll", () => {
+  let scrollY = this.scrollY;
+  if (scrollY >= 2600) {
+    nav.classList.add("text-white");
+    navMenu.classList.add("md:text-white");
+    navMenu.classList.remove("md:text-black");
+  } else {
+    nav.classList.remove("text-white");
+    navMenu.classList.remove("md:text-white");
+    navMenu.classList.add("md:text-black");
+  }
+});
