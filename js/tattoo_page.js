5cd9abb98cc432fe56782fc82dca848488bff059
Ogu <august.gros@gmail.com>
2022-07-21 10:48:43 +0200

    add flash with form auto fill

--- js/tattoo_page.js
+++ js/tattoo_page.js
@@ -1,4 +1,5 @@
 const frames = document.querySelectorAll(".peinture");
+const flashs = document.querySelectorAll(".flash");
 const peintureModal = document.querySelector("#peintureModal");
 const btnCloseModal = document.querySelector("#btnCloseModal");
 const imgModal = document.querySelector("#imgModal");
@@ -10,8 +11,13 @@ const menu = document.querySelector("#menuPeinture");
 const navMenu = document.querySelector("#navMenu");
 const nav = document.querySelector("#nav");
 const linkMenu = document.querySelectorAll(".linkMenu");
+const btnFlash = document.querySelector("#btnFlash");
+const flashModal = document.querySelector("#flashModal");
+const flashChoisi = document.querySelector("#flashChoisi");
+const btnCloseModalFlash = document.querySelector("#btnCloseModalFlash");
+const previewFlash = document.querySelector("#previewFlash");
 
-/* Event pour ouvrir/fermer les boites modale */
+/* Event pour ouvrir les boites modale pour tattoo*/
 frames.forEach((frame) => {
   frame.addEventListener("click", () => {
     imgModal.src = frame.src;
@@ -25,18 +31,51 @@ frames.forEach((frame) => {
   });
 });
 
-/* Croix pour fermer la boite modal */
+/* Event pour ouvrir les boites modale pour flash */
+flashs.forEach((flash) => {
+  flash.addEventListener("click", () => {
+    imgModal.src = flash.src;
+    flashModal.classList.remove("hidden");
+    flashModal.classList.add("flex");
+    flashModal.dataset.modal = "on";
+  });
+
+  btnFlash.addEventListener("click", () => {
+    previewFlash.src = flash.src;
+    window.location.href = "#contact";
+  });
+});
+
+previewFlash.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+
+/* Croix pour fermer la boite modal pour tattoo */
 btnCloseModal.addEventListener("click", () => {
   peintureModal.classList.add("hidden");
   peintureModal.classList.remove("flex");
   peintureModal.dataset.modal = "off";
 });
 
-/* Cliquer dans le vide pour fermer la boite modal */
+/* Cliquer dans le vide pour fermer la boite modal pour tattoo */
 peintureModal.addEventListener("click", () => {
   peintureModal.classList.add("hidden");
   peintureModal.classList.remove("flex");
   peintureModal.dataset.modal = "off";
+});
+
+/* Croix pour fermer la boite modal pour flash */
+btnCloseModalFlash.addEventListener("click", () => {
+  flashModal.classList.add("hidden");
+  flashModal.classList.remove("flex");
+  flashModal.dataset.modal = "off";
+});
+
+/* Cliquer dans le vide pour fermer la boite modal pour flash */
+flashModal.addEventListener("click", () => {
+  flashModal.classList.add("hidden");
+  flashModal.classList.remove("flex");
+  flashModal.dataset.modal = "off";
 });
 
 /* Fonction pour créer des elements */
@@ -79,13 +118,16 @@ const toggleMenu = () => {
 /* Scrpit pour que le textarea resize auto */
 const tx = document.getElementsByTagName("textarea");
 for (let i = 0; i < tx.length; i++) {
-  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+  tx[i].setAttribute(
+    "style",
+    "height:" + tx[i].scrollHeight + "px;overflow-y:hidden;"
+  );
   tx[i].addEventListener("input", OnInput, false);
 }
 
 function OnInput() {
   this.style.height = "auto";
-  this.style.height = (this.scrollHeight) + "px";
+  this.style.height = this.scrollHeight + "px";
 }
 
 /* Preview pour le flash choisis */
