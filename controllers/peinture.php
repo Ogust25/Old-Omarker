f8d6b57aed2f3db45276a1e06fe392cada118908
Ogu <august.gros@gmail.com>
2022-07-27 14:23:23 +0200

    add model for oeuvres

--- controllers/peinture.php
+++ controllers/peinture.php
@@ -1,4 +1,5 @@
 <?php
 
+require_once "./models/oeuvres.php";
 require_once "./models/expositions.php";
 require_once "./views/peinture.php";
---
+++ models/oeuvres.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Connexion pdo */
+include "./models/db_login.php";
+
+$stmt = $db->prepare('SELECT nom, `year`.`year`, technique.technique, taille.taille, `image`.img FROM peinture
+                        INNER JOIN `year` ON `year`.id_year = peinture.id_year
+                        INNER JOIN technique ON technique.id_technique = peinture.id_technique
+                        INNER JOIN taille ON taille.id_taille = peinture.id_taille
+                        INNER JOIN `image` ON `image`.id_image = peinture.id_image
+                        WHERE peinture.`id_type-peinture` = 2');
+$stmt->execute();
+$oeuvres = $stmt->fetchAll(PDO::FETCH_ASSOC);
